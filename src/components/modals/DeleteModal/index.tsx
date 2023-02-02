0530c494ddd800739549b31ef49cb605c282fd5a
Yusuf Numan YILMAZ <ysfnmnylmz@Yusuf-Air.home>
2023-02-02 18:38:20 +0300

    remove modal scss added

--- src/components/modals/DeleteModal/index.tsx
+++ src/components/modals/DeleteModal/index.tsx
@@ -1,32 +1,39 @@
 import React, { type FC } from 'react';
-import { Button, Modal } from 'antd';
+import { Button, Modal, Typography } from 'antd';
 import { useDispatch, useSelector } from 'react-redux';
 import { toggleDeleteModel } from 'store/slices/general';
 import { removeHotel } from 'store/slices/hotels';
+import cn from 'classnames';
+import './DeleteModal.scss';
 
 const DeleteModal: FC = () => {
   const dispatch = useDispatch();
   const generalState = useSelector(({ general }: any) => general);
   const hotelState = useSelector(({ hotels }: any) => hotels);
   const { deleteModal } = generalState;
-  // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-  const handleOk = () => {
+  const handleOk = (): void => {
     dispatch(removeHotel(hotelState.selectedHotel));
+    dispatch(toggleDeleteModel(false));
+  };
+  const handleCancel = (): void => {
     dispatch(toggleDeleteModel(false));
   };
   return (
     <Modal
       title="Hata"
       open={deleteModal}
+      onCancel={handleCancel}
       footer={[
-        <Button key={1} onClick={handleOk}>
-          Hayır
+        <Button className={cn('remove-button')} key={1} type="primary" danger onClick={handleOk}>
+          OTELİ SİL
         </Button>,
-        <Button key={1} type="primary" danger onClick={handleOk}>
-          Evet
+        <Button key={1} className={cn('cancel-button')} onClick={handleCancel}>
+          VAZGEÇ
         </Button>,
       ]}
-    ></Modal>
+    >
+      <Typography>isimli oteli silmek istiyor musun?</Typography>
+    </Modal>
   );
 };
 
